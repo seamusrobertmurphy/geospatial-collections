53e3e1b9a2092fede55f51ee13c53ac327c62e08
Seamus Murphy <seamusrobertmurphy@gmail.com>
2022-05-15 10:31:38 -0700

    setup.sh installed

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ apps = MultiApp()
 
 # Add all your application here
 
-
+apps.add_app("Home", home.app)
 apps.add_app("Create Timelapse", timelapse.app)
 apps.add_app("U.S. Real Estate Data", housing.app)
 apps.add_app("U.S. Census Data", census.app)
@@ -38,11 +38,10 @@ apps.add_app("Heatmaps", heatmap.app)
 apps.add_app("Add Points from XY", xy.app)
 apps.add_app("Add Web Map Service (WMS)", wms.app)
 apps.add_app("Google Earth Engine (GEE)", gee.app)
-apps.add_app("Earth Engine API Collection", gee_datasets.app)
+apps.add_app("Awesome GEE Community Datasets", gee_datasets.app)
 apps.add_app("Geolocation", device_loc.app)
 apps.add_app("Cesium 3D Map", cesium.app)
 apps.add_app("Plotly", plotly_maps.app)
-apps.add_app("Home", home.app)
 
 # The main app
 apps.run()
